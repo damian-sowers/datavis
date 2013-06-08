392abaf48cf603ad69cf58ab9c8235127a1af988
Damian Sowers <damian.sowers@gmail.com>
2013-06-08 14:58:11 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datavis::Application.config.secret_token = '27cf0b4e812abbce647ba8b27ef7e53f34a47df3c56869631848d2cb87028b910fdc3f343324e2f5d8497ebfe33b63bf69f1ad3ec52a85727a70d9b7a22d049b'
